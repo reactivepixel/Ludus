e5c74f99de7ea75728efea57681e82771d8be170
Chris Chapman <cchapman@fullsail.com>
2014-12-26 19:24:13 -0500

    added devise user models

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
-  root to: "home#index"
+  devise_for :users
+  root to: "msgs#index"
   resources :msgs
 
   # The priority is based upon order of creation: first created -> highest priority.
