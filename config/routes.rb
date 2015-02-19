9459c37e3c0da548aea238871958d6cb9086a8b8
Danny O'Carroll <LemonMyth@live.com>
2015-02-18 19:22:13 -0500

    Added new fields to Sign Up

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
 
+  devise_for :users, :controllers => { registrations: 'registrations' }
+
   root to: "channels#index"
   
   resources :channels
-
-  devise_for :users
   
   resources :msgs
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
