6808411f1260433918c556788ea109f21ac1d927
Gabriel Ferraz <gabrielferraz27@gmail.com>
2015-02-23 13:21:48 -0500

    added vimeo to auto_html filters

--- app/models/msg.rb
+++ app/models/msg.rb
@@ -1,8 +1,11 @@
 class Msg < ActiveRecord::Base
   auto_html_for :content do
+  	# see filter options at https://github.com/dejan/auto_html/blob/master/lib/auto_html/filters/soundcloud.rb
+    # see demo: http://rors.org/demos/auto_html
     html_escape
     image
-    youtube(:width => 400, :height => 250, :autoplay => false)
+    vimeo	
+    youtube(:width => 440, :height => 250, :autoplay => false)
     link :target => "_blank", :rel => "nofollow"
     simple_format
   end
