c0b0229f3cd7a5967e4e16e9ac58732684347c6f
Chapman <chapman@apextion.com>
2015-02-23 18:45:56 -0500

    adding reset to deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -53,11 +53,11 @@ set :keep_releases, 2
 namespace :deploy do
 
 	desc 'Restart Application'
-	# task :restart do
-	# 	on roles(:app), in: :sequence, wait: 5 do
-	# 		execute :touch, release_path.join('tmp/restart.txt')
-	# 	end
-	# end
+	task :restart do
+		on roles(:app), in: :sequence, wait: 5 do
+			execute :touch, release_path.join('tmp/restart.txt')
+		end
+	end
 
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
