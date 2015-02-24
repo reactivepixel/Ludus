49438f7bfe3e7e44899b97330e4cb7833e219f38
Chapman <chapman@apextion.com>
2015-02-23 20:09:09 -0500

    Deployments migrating automagically with Cap

--- Capfile
+++ Capfile
@@ -7,7 +7,7 @@ require 'capistrano/deploy'
 
 require 'capistrano/bundler'
 require 'capistrano/rails'
-
+require 'capistrano/rails/migrations'
 
 # Include tasks from other gems included in your Gemfile
 #
@@ -31,7 +31,7 @@ require 'capistrano/rbenv_install'
 # require 'capistrano/chruby'
 # require 'capistrano/bundler'
 # require 'capistrano/rails/assets'
-# require 'capistrano/rails/migrations'
+
 # require 'capistrano/passenger'
 
 # Load custom tasks from `lib/capistrano/tasks' if you have any defined
--- config/deploy.rb
+++ config/deploy.rb
@@ -62,16 +62,16 @@ namespace :deploy do
 		end
 	end
 
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-       within release_path do
-         execute :rake
-       end
-    end
-  end
+  # after :restart, :clear_cache do
+  #   on roles(:web), in: :groups, limit: 3, wait: 10 do
+  #     # Here we can do anything such as:
+  #      within release_path do
+  #        execute :rake
+  #      end
+  #   end
+  # end
 
-  after :publishing, 'deploy:restart'
-  # after :finishing, 'deploy:cleanup'
+  after :publishing, 'deploy:migrate'
+  after :finishing, 'deploy:restart'
 
 end
