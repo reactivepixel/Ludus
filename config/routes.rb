dc35b0ff4560dbe1e63515c87d580dd8ef9e6151
Danny O'Carroll <LemonMyth@live.com>
2015-02-23 19:56:20 -0500

    Added a user list page, as well as profile views

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
 
   devise_for :users, :controllers => { registrations: 'registrations' }
+  resources :users, only: [:index]
 
   root to: "channels#index"
   
   resources :channels
   
   resources :msgs
+
+  resources :users
 
 
 
