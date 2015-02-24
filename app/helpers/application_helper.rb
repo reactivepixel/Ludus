0433425eb06edd6a5ca16275cdb4e4903c955a24
Nicole <nicolecar14@gmail.com>
2015-02-23 23:21:33 -0500

    added a lot of styles. new look.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
 	def avatar_url(user)
 		gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
-		"http://gravatar.com/avatar/#{gravatar_id}.png"
+		"http://gravatar.com/avatar/#{gravatar_id}.png?d=retro"
 	end
 end
