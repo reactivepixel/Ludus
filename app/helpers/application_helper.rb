c56e973f9059606b7d6384ecbe20c601ef20fe68
Gabriel Ferraz <gabrielferraz27@gmail.com>
2015-02-27 15:34:26 -0500

    commented unused markdown code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,20 +4,20 @@ module ApplicationHelper
 		"http://gravatar.com/avatar/#{gravatar_id}.png?d=retro"
 	end
 
-	def markdown(text)
-		options = {
-			hard_wrap: true,
-			autolink: true, 
-			quote: true, 
-			fenced_code_blocks: true, 
-			strikethrough: true, 
-			underline: true, 
-			highlight: true, 
-			quote: true
-		}
-		markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, options)
-		return markdown.render(text).html_safe
-	end
+	# def markdown(text)
+	# 	options = {
+	# 		hard_wrap: true,
+	# 		autolink: true, 
+	# 		quote: true, 
+	# 		fenced_code_blocks: true, 
+	# 		strikethrough: true, 
+	# 		underline: true, 
+	# 		highlight: true, 
+	# 		quote: true
+	# 	}
+	# 	markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, options)
+	# 	return markdown.render(text).html_safe
+	# end
 
 end
 
