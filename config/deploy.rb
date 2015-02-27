708b759815759f311575a6dbc883a3bca855b938
Chapman <chapman@apextion.com>
2015-02-26 22:01:47 -0500

    env vars gem removed

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,7 +20,7 @@ set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rben
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :rbenv_roles, :all # default value
 
-set :linked_files, %w{config/database.yml}
+# set :linked_files, %w{config/database.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 set :keep_releases, 5
