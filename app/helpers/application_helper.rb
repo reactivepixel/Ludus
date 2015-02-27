346eb83c72646e6ffd377d42f38a1bb6f2ec9365
Gabriel Ferraz <gabrielferraz27@gmail.com>
2015-02-27 18:52:34 -0500

    added remote

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,19 +7,19 @@ module ApplicationHelper
 
 	#  code below is not being used in this implementation; gem auto_html provides a filter redcarpet that allows to use markdown; using that instead (see msg model)
 
-	# def markdown(text)
-	# 	options = {
-	# 		hard_wrap: true,
-	# 		autolink: true, 
-	# 		quote: true, 
-	# 		fenced_code_blocks: true, 
-	# 		strikethrough: true, 
-	# 		underline: true, 
-	# 		highlight: true, 
-	# 		quote: true
-	# 	}
-	# 	markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, options)
-	# 	return markdown.render(text).html_safe
-	# end
+	def markdown(text)
+		options = {
+			hard_wrap: true,
+			autolink: true, 
+			quote: true, 
+			fenced_code_blocks: true, 
+			strikethrough: true, 
+			underline: true, 
+			highlight: true, 
+			quote: true
+		}
+		markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, options)
+		return markdown.render(text).html_safe
+	end
 end
 
