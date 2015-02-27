993ef78951b28474ebb1a1af10562a6d920142e9
Danny O'Carroll <LemonMyth@live.com>
2015-02-27 17:28:25 -0500

    All kinds of Github repo and username junk

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -7,7 +7,7 @@ class RegistrationsController < Devise::RegistrationsController
   end
  
   def account_update_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password, :color, :repo_name)
   end
 
 
