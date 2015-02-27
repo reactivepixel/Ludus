44c38ed6b19a5cea6942bf7b305cc75b0fcea38a
Gabriel Ferraz <gabrielferraz27@gmail.com>
2015-02-27 14:10:10 -0500

    added more filters to markdown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,7 @@ module ApplicationHelper
 	end
 
 	def markdown(text)
-		markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, hard_wrap: true, autolink: true, quote: true, fenced_code_blocks: true, strikethrough: true)
+		markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, hard_wrap: true, autolink: true, quote: true, fenced_code_blocks: true, strikethrough: true, underline: true, highlight: true)
 		return markdown.render(text).html_safe
 	end
 
